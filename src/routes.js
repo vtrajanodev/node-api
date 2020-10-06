43ddf5ab25776fd9fdc1435a341aea0b1168614f
Victor Trajano <trajanodev@hotmail.com>
2020-10-05 23:07:47 -0400

    Atualização da rota index

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = express.Router()
 const ProductController = require('./controller/ProductController')
 
 
-routes.get('/products' , ProductController.index)
+routes.get('/' , ProductController.index)
 routes.get('/products/:id' , ProductController.getById)
 routes.post('/products' , ProductController.register) 
 routes.put('/products/:id' , ProductController.update)
