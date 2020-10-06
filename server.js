d595dc8ac238fac76ecafb5cd837c3bd91429da8
Victor Trajano <trajanodev@hotmail.com>
2020-10-05 22:18:40 -0400

    Atualizando para deploy

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.use(express.json())
 app.use(cors());
 
 //Iniciando database
-mongoose.connect('mongodb://127.0.0.1:27017/newbackend' , 
+// mongoose.connect('mongodb://127.0.0.1:27017/newbackend' , 
+// {useNewUrlParser: true , useFindAndModify: false  , useUnifiedTopology: true})
+
+mongoose.connect('mongodb+srv://deploy:3966120vic@dbproduct.xw0dz.mongodb.net/dbproduct?retryWrites=true&w=majority' , 
 {useNewUrlParser: true , useFindAndModify: false  , useUnifiedTopology: true})
 
 
